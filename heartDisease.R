60872f0a1a57e7ff7d12eb16751dd812339d7b85
topty <topty@live.com>
2019-05-01 21:57:11 +0700

    read + filter data

---
+++ heartDisease.R
@@ -0,0 +1,48 @@
+library(tidyverse)
+
+field <- c(
+  'age',
+  'sex',
+  'cp',
+  'trestbps',
+  'chol',
+  'fbs',
+  'restecg',
+  'thalach',
+  'exang',
+  'oldpeak',
+  'slope',
+  'ca',
+  'thal',
+  'target',
+  'location'
+)
+
+# read data from csv
+cleveland <- read.csv('processed.cleveland.data', header = FALSE)
+hungarian <- read.csv('processed.hungarian.data', header = FALSE)
+switzerland <- read.csv('processed.switzerland.data', header = FALSE)
+va <- read.csv('processed.va.data', header = FALSE, stringsAsFactors = FALSE)
+
+# set location
+cleveland$V15 <- 'cleveland'
+hungarian$V15 <- 'hungarian'
+switzerland$V15 <- 'switzerland'
+va$V15 <- 'va'
+
+# stack data
+data <- rbind(cleveland, hungarian, switzerland, va)
+
+# set column name
+colnames(data) <- field
+
+# change '?' to NA
+data[data == '?'] <- NA
+
+# change column 1 to 14 to number
+for(i in 1:14) {
+  data[, i] <- as.numeric(data[, i])
+}
+
+# remove NA
+data <- data[complete.cases(data), ]
