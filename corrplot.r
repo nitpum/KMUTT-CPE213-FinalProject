dff1d52cd457a6ddb0ab263d7dc2d13647fde1d4
topty <topty@live.com>
2019-05-03 10:01:34 +0700

    change preparation code

--- corrplot.r
+++ corrplot.r
@@ -12,8 +12,8 @@ preData <- read.csv('csv/weatherAUS.csv')
 # Data Preparation
 #
 #
+# Cleaning
 preData %>%
-  # Cleaning
   select(-WindDir3pm, -WindDir9am, -WindGustDir, -Rainfall, -Location, -RISK_MM) %>% 
   filter(!is.na(RainTomorrow), !is.na(RainToday)) -> preData
 
@@ -23,28 +23,23 @@ preData %>%
   mutate(Year = as.numeric(Year), Month = as.numeric(Month), Day = as.numeric(Day)) %>%
   mutate(MonthName = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")[Month]) -> preData
 
-preData$RainToday<-str_replace_all(preData$RainToday,"No","0")
-preData$RainToday<-str_replace_all(preData$RainToday,"Yes","1")
-preData$RainTomorrow<-str_replace_all(preData$RainTomorrow,"No","0")
-preData$RainTomorrow<-str_replace_all(preData$RainTomorrow,"Yes","1")
 preData$MinTemp <- as.numeric(preData$MinTemp)
-preData$RainToday<-as.numeric(preData$RainToday)
-preData$RainTomorrow<-as.numeric(preData$RainTomorrow)
-preData$MinTemp[which(is.na(preData$MinTemp))] <- mean(preData$MinTemp,na.rm = TRUE)
-preData$MaxTemp[which(is.na(preData$MaxTemp))] <- mean(preData$MaxTemp,na.rm = TRUE)
-preData$WindGustSpeed[which(is.na(preData$WindGustSpeed))] <- mean(preData$WindGustSpeed,na.rm = TRUE)
-preData$WindSpeed9am[which(is.na(preData$WindSpeed9am))] <- mean(preData$WindSpeed9am,na.rm = TRUE)
-preData$WindSpeed3pm[which(is.na(preData$WindSpeed3pm))] <- mean(preData$WindSpeed3pm,na.rm = TRUE)
-preData$Cloud3pm[which(is.na(preData$Cloud3pm))] <- mean(preData$Cloud3pm,na.rm = TRUE)
-preData$Cloud9am[which(is.na(preData$Cloud9am))] <- mean(preData$Cloud9am,na.rm = TRUE)
-preData$Humidity3pm[which(is.na(preData$Humidity3pm))] <- mean(preData$Humidity3pm,na.rm = TRUE)
-preData$Humidity9am[which(is.na(preData$Humidity9am))] <- mean(preData$Humidity9am,na.rm = TRUE)
-preData$Pressure9am[which(is.na(preData$Pressure9am))] <- mean(preData$Pressure9am,na.rm = TRUE)
-preData$Pressure3pm[which(is.na(preData$Pressure3pm))] <- mean(preData$Pressure3pm,na.rm = TRUE)
-preData$Temp9am[which(is.na(preData$Temp9am))] <- mean(preData$Temp9am,na.rm = TRUE)
-preData$Temp3pm[which(is.na(preData$Temp3pm))] <- mean(preData$Temp3pm,na.rm = TRUE)
-preData$Sunshine[which(is.na(preData$Sunshine))] <- mean(preData$Sunshine, na.rm = TRUE)
-preData$Evaporation[which(is.na(preData$Evaporation))] <- mean(preData$Evaporation, na.rm = TRUE)
+
+# change yes/no to 1/0
+preData$RainToday <- ifelse(RainToday == 'Yes', 1, 0)
+preData$RainTomorrow <- ifelse(RainTomorrow == 'Yes', 1, 0)
+
+# replace na with mean
+for(col in c('MinTemp', 'MaxTemp', 'WindGustSpeed', 'Sunshine', 'Evaporation')) {
+  preData[which(is.na(preData[, col])), col] <- mean(preData[, col], na.rm = TRUE)
+}
+
+for(colname in c('WindSpeed', 'Cloud', 'Humidity', 'Pressure', 'Temp')) {
+  for(time in c('9am', '3pm')) {
+    col <- paste(colname, time, sep = '')
+    preData[which(is.na(preData[, col])), col] <- mean(preData[, col], na.rm = TRUE)
+  }
+}
 
 preData -> data
 
